6ddc2a108d3e0fc3b723e76acf999a344085c1b6
scarlettnik <87085957+scarlettnik@users.noreply.github.com>
2023-07-05 01:41:55 +0300

    Update index.js

--- src/authorize/index.js
+++ src/authorize/index.js
@@ -3,7 +3,7 @@ const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 const REDIRECT_URL_AFTER_LOGIN = process.env. REACT_APP_REDIRECT_URL; const SPACE_DELIMITER = "%20";
 const SCOPES = [
-"user-top-read", // for getting the user's ton tracks and artists
+"user-top-read",
 "user-read-private",
 "user-library-read",
 "playlist-read-private",
